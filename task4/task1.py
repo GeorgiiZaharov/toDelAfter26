1ce4720ae5a3ea95c8d422da30b27c20f441ed69
GeorgiiZaharov <zgr05.gz@gmail.com>
2024-06-25 23:52:43 +0300

    ADD FILES

--- task4/task1.py
+++ task4/task1.py
@@ -1,9 +1,9 @@
 nums = [-2,1,-3,4,-1,2,1,-5,4]
-nums = [5, 4, -1, 7, 8]
-nums = [-42]
-res, cur_sum, min_zona = 0, 0, 0
-for i in nums:
-    cur_sum += i
-    min_zona = min(min_zona, cur_sum)
-    res = max(res, cur_sum - min_zona)
+# nums = [5, 4, -1, 7, 8]
+# nums = [-42]
+res = 0
+# перебираем все подотрезки
+for i in range(len(nums)):
+    for j in range(i, len(nums) + 1):
+        res = max(sum(nums[i: j]), res)
 print(res)
